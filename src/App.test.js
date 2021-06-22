b61579217061c7a7afd46f5e529c94fb6c4077db
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-22 18:10:44 -0400

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('Renders w/o error!', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
